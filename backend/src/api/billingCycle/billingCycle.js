3d82b02a6195e5c7771c7ea9f12e146d269aab8d
ciro stodulski de azevedo <ciro.sda@gmail>
2018-05-30 23:02:36 -0300

    backend, instalation of dependecys, create loader and schema

---
+++ backend/src/api/billingCycle/billingCycle.js
@@ -0,0 +1,23 @@
+const restful = require('node-restful');
+const mongoose = restful.mongoose;
+
+const creditScherma = new mongoose.Schema({
+    name: { type: String, required: true },
+    value: { type: Number, min: 0, required: true }
+});
+
+const debtSchema = new mangoose.Schema({
+    name: { type: String, require: true },
+    value: { type: Number, min: 0, required: true },
+    status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PEDENTE', 'AGENDADO'] }
+});
+
+const billingCycleSchema = new mangoose.Schema({
+    name: { type: String, required: true },
+    month: { type: Number, min: 1, max: 12, required: true },
+    year: { type: Number, min: 1970, max: 2100, required: true },
+    credits: [creditScherma],
+    debts: [debtSchema]
+});
+
+module.exports = restful.model('BillingCycle', billingCycleSchema);
